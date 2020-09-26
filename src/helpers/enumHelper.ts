0426c7987807a90adba1f4b5ed7d65026f4440b5
Roman Novozhilov <romannovojilov@mail.ru>
2020-09-27 00:15:03 +0300

    opt helper

--- src/helpers/enumHelper.ts
+++ src/helpers/enumHelper.ts
@@ -1,6 +1,7 @@
 export const enumHelper = {
   map<R, E>(targetEnum: E | Object, callback: (value: string, key: number) => R) {
-    const result: R[] = Object.keys(targetEnum).slice(0, Object.keys(targetEnum).length / 2).map(key => {
+    const keys = Object.keys(targetEnum);
+    const result: R[] = keys.slice(0, keys.length / 2).map(key => {
       const numericKey = Number(key);
       const value = (targetEnum as any[])[numericKey];
       const newValue = callback(value, numericKey);
